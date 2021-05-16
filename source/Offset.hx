79ae7a9b9def997ca06d6720ec10dee62a6d9c4f
bbsub3 <84255841+bbsub3@users.noreply.github.com>
2021-05-15 21:50:07 -0400

    resta code

---
+++ source/Offset.hx
@@ -0,0 +1,23 @@
+package;
+
+/**
+ * ...
+ * @author ...
+ */
+typedef SwagOffset =
+{
+	var x:Float;
+	var y:Float;
+	var name:String;
+}
+class Offset 
+{
+	public var x:Float;
+	public var y:Float;
+	public var name:String;
+	public function new() 
+	{
+		
+	}
+	
+}
